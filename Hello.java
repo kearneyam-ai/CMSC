c763495858c2b3983c6b049b7bd36110b121a782
kearneyam-ai <69979046+kearneyam-ai@users.noreply.github.com>
2020-08-20 11:40:43 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*************************************************
+*Hello Java Test
+**************************************************
+*Ashley Kearney
+*8-20-2020
+*CMSC 255 Section 001
+**************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Ashley Kearney");
+		System.out.println("Hello Java Test");
+		System.out.println("Testing CMD Functionality");
+		System.out.println("August 20th 2020");
+		System.out.println("CMSC 255 Section 001");
+	}
+}
